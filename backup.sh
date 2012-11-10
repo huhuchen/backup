782d5b435e50cc2fda0127fd72124bc19f39e4b4
huhuchen <chengengtiger@gmail.com>
2012-11-10 14:34:19 +0800

    fix bug for the result of bash md5 check

--- backup.sh
+++ backup.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
+
+##define varibale
 remote_addr=stdyun@218.245.3.70:~/stdyun.backup/ 
 now=$(date +%Y%m%d)
+
+##backup table
 python backup_table.py
 scp  *${now}.sql ${remote_addr} 
+
+##backup data
 python backup_data.py
 scp  *${now}.sql.lzma ${remote_addr}
+
+##create the md5 of backup file
 md5sum *${now}.sql.lzma *${now}.sql  > backup_${now}.md5
 scp backup_${now}.md5 ${remote_addr}
+
+##delete expired data
 old_date=$(date -d "7 days ago" +%Y%m%d)
 find . -name "*${old_date}*" -exec rm {} \;
--- md5_check.sh
+++ md5_check.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+
 now=$(date +%Y%m%d)
+
 message=$(md5sum -c backup_${now}.md5)
+
 result=$(echo $?)
-if [ ! result ];then
+
+if [ "$result" == "0" ];then
     echo "$message"
 fi
